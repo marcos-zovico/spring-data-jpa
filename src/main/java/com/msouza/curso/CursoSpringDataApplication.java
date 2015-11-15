dac3a46b67f263d350efb7f46a9653aa6fe02508
MarcosAntonio <MarcosAntonio@192.168.0.11>
2015-11-15 21:28:00 -0200

    Salvando e excluindo em lote

--- src/main/java/com/msouza/curso/CursoSpringDataApplication.java
+++ src/main/java/com/msouza/curso/CursoSpringDataApplication.java
@@ -43,18 +43,78 @@ public class CursoSpringDataApplication implements CommandLineRunner {
 	public void run(String... arg0) throws Exception {
 		// testConfiguration();
 		// testSave();
-//		testUpdate();
-		testDelete();
+		// testUpdate();
+		// testDelete();
+		
+//		testSavePersons();
+		testDeletePersons();
+	}
+
+	private void testDeletePersons() {
+		Person p1 = personRepository.findOne(14L);
+		Person p2 = personRepository.findOne(15L);
+		Person p3 = personRepository.findOne(16L);
+		
+		personRepository.delete(Arrays.asList(p1, p2, p3));
+		
+		System.out.println("========================================");
+		
+		Person p4 = personRepository.findOne(17L);
+		Person p5 = personRepository.findOne(18L);
+		Person p6 = personRepository.findOne(19L);
+		
+		personRepository.deleteInBatch(Arrays.asList(p4, p5, p6));
+	}
+
+	private void testSavePersons() {
+		Person p1 = new Person();
+		p1.setFirstName("Bruno");
+		p1.setLastName("Pereira de Melo");
+		p1.setAge(63);
+		p1.setDocument(new Document("751.467.964-00", 345444249));
+		
+		
+		Person p2 = new Person();
+		p2.setFirstName("Alison");
+		p2.setLastName("Souza do caralho");
+		p2.setAge(25);
+		p2.setDocument(new Document("741.467.800-65", 345680000));
+	
+		Person p3 = new Person();
+		p3.setFirstName("Carlos Ricardo");
+		p3.setLastName("Pereira");
+		p3.setAge(45);
+		p3.setDocument(new Document("743.467.111-75", 34555555));
+	
+		Person p4 = new Person();
+		p4.setFirstName("Fábio");
+		p4.setLastName("Guimarães");
+		p4.setAge(25);
+		p4.setDocument(new Document("711.467.444-90", 34113249));
+		
+		Person p5 = new Person();
+		p5.setFirstName("Vinícius");
+		p5.setLastName("Pereira");
+		p5.setAge(25);
+		p5.setDocument(new Document("741.467.333-33", 345683233));
+	
+		Person p6 = new Person();
+		p6.setFirstName("Carla");
+		p6.setLastName("Hibas");
+		p6.setAge(28);
+		p6.setDocument(new Document("331.467.222-65", 333383249));
+	
+		List<Person> persons = personRepository.save(Arrays.asList(p1, p2,p3, p4, p5, p6));
+	
+	persons.forEach(System.out::println);
 	}
 
 	private void testDelete() {
 		personRepository.delete(13L);
-		
+
 		Person person = personRepository.findOne(12L);
 		personRepository.delete(person);
-		
-		
-		
+
 	}
 
 	private void testUpdate() {
@@ -67,7 +127,7 @@ public class CursoSpringDataApplication implements CommandLineRunner {
 		Person p2 = personRepository.findOne(13L);
 
 		System.out.println(p2.toString());
-		
+
 		List<Person> persons = personRepository.findAll();
 		persons.forEach(System.out::println);
 	}
