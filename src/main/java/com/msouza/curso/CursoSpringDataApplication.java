305fca9233403ef95ac0317561076be60c74bfee
MarcosAntonio <MarcosAntonio@192.168.0.11>
2015-11-16 22:10:53 -0200

    Métodos de consultas herdados de JpaRepository

--- src/main/java/com/msouza/curso/CursoSpringDataApplication.java
+++ src/main/java/com/msouza/curso/CursoSpringDataApplication.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 
 import com.msouza.curso.entity.Address;
 import com.msouza.curso.entity.Address.TypeAddres;
-import com.msouza.curso.entity.Phone.TypePhone;
 import com.msouza.curso.entity.Document;
 import com.msouza.curso.entity.Person;
 import com.msouza.curso.entity.Phone;
+import com.msouza.curso.entity.Phone.TypePhone;
 import com.msouza.curso.repository.AddressRepository;
 import com.msouza.curso.repository.DocumentRepository;
 import com.msouza.curso.repository.PersonRepository;
@@ -45,24 +50,66 @@ public class CursoSpringDataApplication implements CommandLineRunner {
 		// testSave();
 		// testUpdate();
 		// testDelete();
+		// testSavePersons();
+		// testDeletePersons();
+		// testFindAndSort();
+//		testFindByIds();
+//		testExists();
+		testPagination();
 		
-//		testSavePersons();
-		testDeletePersons();
+	}
+
+	private void testPagination() {
+		
+   Page<Person> pages = personRepository.findAll(new PageRequest(0,2));
+   pages.getContent().forEach(System.out::println);
+   
+   pages = personRepository.findAll(new PageRequest(1,2));
+   pages.getContent().forEach(System.out::println);
+   
+   pages = personRepository.findAll(new PageRequest(2,2));
+   pages.getContent().forEach(System.out::println);
+   	
+	}
+
+	private void testExists() {
+		boolean p1 = personRepository.exists(5L);
+		System.out.println("P1 is " + p1);
+		
+		boolean p2 = personRepository.exists(50L);
+		System.out.println("P2 is " + p2);
+		
+	}
+
+	private void testFindByIds() {
+		List<Person> persons = personRepository.findAll(Arrays.asList(1L, 5L, 2L, 6L));
+		persons.forEach(System.out::println);
+	}
+
+	private void testFindAndSort() {
+		Order orderAsc = new Order(Direction.ASC, "lastName");
+
+		Order orderDesc = new Order(Direction.ASC, "firstName");
+		Sort sort = new Sort(orderAsc, orderDesc);
+		List<Person> persons = personRepository.findAll(sort);
+
+		persons.forEach(System.out::println);
+
 	}
 
 	private void testDeletePersons() {
 		Person p1 = personRepository.findOne(14L);
 		Person p2 = personRepository.findOne(15L);
 		Person p3 = personRepository.findOne(16L);
-		
+
 		personRepository.delete(Arrays.asList(p1, p2, p3));
-		
+
 		System.out.println("========================================");
-		
+
 		Person p4 = personRepository.findOne(17L);
 		Person p5 = personRepository.findOne(18L);
 		Person p6 = personRepository.findOne(19L);
-		
+
 		personRepository.deleteInBatch(Arrays.asList(p4, p5, p6));
 	}
 
@@ -72,41 +119,40 @@ public class CursoSpringDataApplication implements CommandLineRunner {
 		p1.setLastName("Pereira de Melo");
 		p1.setAge(63);
 		p1.setDocument(new Document("751.467.964-00", 345444249));
-		
-		
+
 		Person p2 = new Person();
 		p2.setFirstName("Alison");
 		p2.setLastName("Souza do caralho");
 		p2.setAge(25);
 		p2.setDocument(new Document("741.467.800-65", 345680000));
-	
+
 		Person p3 = new Person();
 		p3.setFirstName("Carlos Ricardo");
 		p3.setLastName("Pereira");
 		p3.setAge(45);
 		p3.setDocument(new Document("743.467.111-75", 34555555));
-	
+
 		Person p4 = new Person();
 		p4.setFirstName("Fábio");
 		p4.setLastName("Guimarães");
 		p4.setAge(25);
 		p4.setDocument(new Document("711.467.444-90", 34113249));
-		
+
 		Person p5 = new Person();
 		p5.setFirstName("Vinícius");
 		p5.setLastName("Pereira");
 		p5.setAge(25);
 		p5.setDocument(new Document("741.467.333-33", 345683233));
-	
+
 		Person p6 = new Person();
 		p6.setFirstName("Carla");
 		p6.setLastName("Hibas");
 		p6.setAge(28);
 		p6.setDocument(new Document("331.467.222-65", 333383249));
-	
-		List<Person> persons = personRepository.save(Arrays.asList(p1, p2,p3, p4, p5, p6));
-	
-	persons.forEach(System.out::println);
+
+		List<Person> persons = personRepository.save(Arrays.asList(p1, p2, p3, p4, p5, p6));
+
+		persons.forEach(System.out::println);
 	}
 
 	private void testDelete() {
